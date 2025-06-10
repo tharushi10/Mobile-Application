52f28e892bf5f79cf5bb2fc3cc0cbcf7a4c12b4e
Tharushi <163126235+tharushi10@users.noreply.github.com>
2025-06-10 18:40:46 +0530

    splash screen added

--- app/src/main/java/com/example/mobileapplication/MainActivity.java
+++ app/src/main/java/com/example/mobileapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Set padding for system bars
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Delay and move to login screen after 2 seconds
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, signinscreen.class);
+            startActivity(intent);
+            finish(); // Close splash screen so user can't go back to it
+        }, 2000); // 2000 milliseconds = 2 seconds
     }
 }
