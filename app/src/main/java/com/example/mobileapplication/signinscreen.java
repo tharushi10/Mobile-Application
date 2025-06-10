b92f8378f27fdeb959be8c48d776a65c2a3a6410
Tharushi <163126235+tharushi10@users.noreply.github.com>
2025-06-10 18:47:22 +0530

    sign in screen added

---
+++ app/src/main/java/com/example/mobileapplication/signinscreen.java
@@ -0,0 +1,40 @@
+package com.example.mobileapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class signinscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signinscreen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView signUpLink = findViewById(R.id.textView);
+        signUpLink.setOnClickListener(v -> {
+            Intent intent = new Intent(signinscreen.this, signupscreen.class);
+            startActivity(intent);
+        });
+
+        Button loginButton = findViewById(R.id.btnLogin);
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(signinscreen.this, newsscreen.class);
+            startActivity(intent);
+        });
+    }
+}
